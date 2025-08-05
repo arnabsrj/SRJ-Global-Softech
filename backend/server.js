3c0db53508ed42de2d7c9b75337ecfa5065216b6
arnabsrj <srjglobal.arnab.fullstack@gmail.com>
2025-08-05 16:47:42 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -28,10 +28,7 @@ app.use("/api/payment", paymentRoutes);
 
 // MongoDB Connection
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("✅ MongoDB connected");
     app.listen(PORT, () => {
